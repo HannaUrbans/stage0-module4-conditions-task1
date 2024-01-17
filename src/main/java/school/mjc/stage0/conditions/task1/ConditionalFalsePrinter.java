b73f79a264842b8213baee6387d33edb06c5020c
Hanna_Urban <huan92@yandex.ru>
2024-01-17 16:49:13 +0300

    some problems still need to be corrected

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        boolean a=false;
+        if (a != false) {
+        }
+        else{ System.out.println("false");
+        }
     }
 }
+
+//Create a program that will print "false" to console from else part of "if" block.
+// Printing command should be placed inside else block,
+// whereas "if" condition will have "false" in condition part.
--- src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDeterminer.java
@@ -1,6 +1,19 @@
 package school.mjc.stage0.conditions.task1;
 
 public class PositiveNumberDeterminer {
-    public void isPositive(int numberToBeDetermined) {
+    public static void isPositive(int numberToBeDetermined) {
+        if(numberToBeDetermined>0){
+            System.out.println("true");
+        }
+        else{System.out.println("false");}}
+
+    public static void main (String [] args){
+        int numberToBeDetermined=5;
+        isPositive (numberToBeDetermined);
     }
+
 }
+
+//Create a program that will determine and print
+// if consumed number is positive (in this case print true) or
+// no (print false) using if-else block.
